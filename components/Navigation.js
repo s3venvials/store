6b0c619e3684f28373d0ac20f44c188133c7a6a8
Mark White <qa,otto.hub@gmail.com>
2021-02-14 15:34:40 -0700

    Update how cart is updated in nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,17 @@
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 export default function Navigation(props) {
   const router = useRouter();
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if (props.cart !== null) {
+      console.log(props.cart.length);
+      setTotal(props.cart.length);
+    }
+  }, [props.cart]);
+
   return (
     <nav className="navbar navbar-expand-lg sticky-top navbar-light bg-light">
       <div className="container-fluid">
@@ -26,9 +36,7 @@ export default function Navigation(props) {
               src="https://res.cloudinary.com/frontndev/image/upload/v1613262319/shopping-cart_pmrkym.png"
               alt="cart"
             />{" "}
-            <span className="badge bg-info">
-              {props.cart.length ? props.cart.length : 0}
-            </span>
+            <span className="badge bg-info">{total}</span>
           </button>
         </div>
       </div>
